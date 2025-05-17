fc2e8c5a5300a04cb1750913c522f12d76d1ec11
DacacodeX <castillo089FROSTY@gmail.com>
2025-05-17 13:37:18 -0500

    refactor(entity.model): add validation for entity type and attributes
    Add validation rules for the `type` field to ensure it is either 'NATURAL' or 'JURIDICA' and not null. Introduce conditional validations for `firstName`, `lastName`, and `businessName` based on the entity type. Remove the `name` field and add `description` field as optional.

--- src/models/entity.model.js
+++ src/models/entity.model.js
@@ -11,13 +11,18 @@ module.exports = (sequelize) => {
   }
 
   Entity.init({
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
     type: {
       type: DataTypes.ENUM('NATURAL', 'JURIDICA'),
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'El tipo de entidad es requerido'
+        },
+        isIn: {
+          args: [['NATURAL', 'JURIDICA']],
+          msg: 'El tipo de entidad debe ser NATURAL o JURIDICA'
+        }
+      }
     },
     documentNumber: {
       type: DataTypes.STRING,
@@ -34,8 +39,38 @@ module.exports = (sequelize) => {
         }
       }
     },
-    description: {
-      type: DataTypes.TEXT
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        isValidForNatural() {
+          if (this.type === 'NATURAL' && !this.firstName) {
+            throw new Error('El nombre es requerido para personas naturales');
+          }
+        }
+      }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        isValidForNatural() {
+          if (this.type === 'NATURAL' && !this.lastName) {
+            throw new Error('El apellido es requerido para personas naturales');
+          }
+        }
+      }
+    },
+    businessName: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        isValidForJuridica() {
+          if (this.type === 'JURIDICA' && !this.businessName) {
+            throw new Error('La razón social es requerida para personas jurídicas');
+          }
+        }
+      }
     },
     active: {
       type: DataTypes.BOOLEAN,
